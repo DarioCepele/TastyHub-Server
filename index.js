f118ce4e1ee71f23f32b98016485e5322b10c74e
Dario Cepele <64326626+DarioCepele@users.noreply.github.com>
2024-02-04 12:42:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const morgan = require("morgan");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+mongoose.connect(process.env.DATABASE_URL);
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to Database"));
