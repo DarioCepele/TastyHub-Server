84fa137cc8fa23084ef3194e92f2ddf4d8ffe1ca
Dario Cepele <64326626+DarioCepele@users.noreply.github.com>
2024-03-22 13:00:58 +0100

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const UserController = require("../controllers/userController");
-const authenticateLogin = require("../middlewares/authenticateLogin");
 
 router.post("/", UserController.createUser);
-router.post("/login", authenticateLogin, UserController.login);
+router.post("/login", UserController.login);
 
 module.exports = router;
