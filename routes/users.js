b6b6622197fec26f28f4581192be6fd730cf7395
Dario Cepele <64326626+DarioCepele@users.noreply.github.com>
2024-03-22 10:14:16 +0100

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,5 @@ const UserController = require('../controllers/userController');
 router.get('/', UserController.getUsers);
 router.post('/', UserController.createUser);
 router.post('/login', UserController.login)
-router.delete('/:userId', UserController.deleteUser);
 
 module.exports = router;
