ffdca6a8322beaa29f118d2c6aa9766d1065d3bf
Dario Cepele <64326626+DarioCepele@users.noreply.github.com>
2024-03-22 10:13:59 +0100

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -53,18 +53,6 @@ exports.login = async (req, res) => {
   }
 };
 
-exports.deleteUser = async (req, res) => {
-  try {
-    const user = await User.findByIdAndDelete(req.params.userId);
-    if (!user) {
-      return res.status(404).json({ error: "Utente non trovato" });
-    }
-    res.json({ message: "Utente cancellato con successo" });
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-};
-
 exports.addRecipe = async (req, res) => {
   try {
     const { id, title, imgae } = req.recipes.body;
